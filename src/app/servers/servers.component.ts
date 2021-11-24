6cbc50a1d128e070f6a507d825cfde7f6aec16fc
Hussein <husseinm1818@gmail.com>
2021-11-24 15:44:45 +0200

    finshed one-way binding

---
+++ src/app/servers/servers.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-servers',
+  templateUrl: './servers.component.html',
+  styleUrls: ['./servers.component.css'],
+})
+export class ServersComponent implements OnInit {
+  public allowNewServer: boolean = false;
+  public serverCreationStatus: string = 'No server was created!';
+  public serverName: string = '';
+
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+  }
+
+  ngOnInit(): void {}
+
+  onCreateServer(): void {
+    this.serverCreationStatus = 'Server was created!';
+  }
+
+  onUpdateServerName(event: Event): void {
+    this.serverName = (<HTMLInputElement>event.target).value;
+  }
+}
