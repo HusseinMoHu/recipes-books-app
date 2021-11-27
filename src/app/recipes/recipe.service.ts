60744085b723dd4f3026746b16a725c99354f930
Hussein <husseinm1818@gmail.com>
2021-11-27 23:58:58 +0200

    finshed services

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,4 +1,6 @@
 import { EventEmitter, Injectable } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
 import { Recipe } from './recipe.model';
 
 @Injectable({
@@ -6,28 +8,48 @@ import { Recipe } from './recipe.model';
 })
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
+
   private recipes: Recipe[] = [
     new Recipe(
-      'A Test Recipe',
+      'Tasty Schnitzel',
       'This is simply a test',
-      'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'
+      'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg',
+      [
+        new Ingredient('Meat', 1),
+        new Ingredient('French Fries', 20),
+        new Ingredient('Bread', 2),
+      ]
     ),
     new Recipe(
-      'Another Test Recipe',
+      'Big Fat Burger',
       'This is simply a test',
-      'https://upload.wikimedia.org/wikipedia/commons/thumb/e/ed/Filet_de_truite_aux_baies_roses_au_restaurant_La_Cocagne_%28Lyon%29.jpg/640px-Filet_de_truite_aux_baies_roses_au_restaurant_La_Cocagne_%28Lyon%29.jpg'
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/e/ed/Filet_de_truite_aux_baies_roses_au_restaurant_La_Cocagne_%28Lyon%29.jpg/640px-Filet_de_truite_aux_baies_roses_au_restaurant_La_Cocagne_%28Lyon%29.jpg',
+      [
+        new Ingredient('Buns', 2),
+        new Ingredient('Meat', 3),
+        new Ingredient('Cheese', 4),
+      ]
     ),
     new Recipe(
-      'More Test Recipe',
+      'Pumpkin Seeds',
       'This is simply a test',
-      'https://upload.wikimedia.org/wikipedia/commons/thumb/7/7a/African_recipes.jpg/640px-African_recipes.jpg'
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/7/7a/African_recipes.jpg/640px-African_recipes.jpg',
+      [
+        new Ingredient('Pumpkin Seeds', 1),
+        new Ingredient('Sugar', 2),
+        new Ingredient('Salt', 3),
+      ]
     ),
   ];
 
-  constructor() {}
+  constructor(private slService: ShoppingListService) {}
 
   getRecipes() {
     // slice() returns a copy of the array, so we don't modify the original array
     return this.recipes.slice();
+  }
+
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
+    this.slService.addIngredients(ingredients);
   }
 }
--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,12 +1,6 @@
-import {
-  Component,
-  ElementRef,
-  OnInit,
-  ViewChild,
-  EventEmitter,
-  Output,
-} from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { Ingredient } from 'src/app/shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list.service';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -17,10 +11,7 @@ export class ShoppingEditComponent implements OnInit {
   @ViewChild('nameInput') nameInputRef: ElementRef;
   @ViewChild('amountInput') amountInputRef: ElementRef;
 
-  // add event emitter, and expose it to the parent component through the @Output decorator
-  @Output() ingredientAdded = new EventEmitter<Ingredient>();
-
-  constructor() {}
+  constructor(private slService: ShoppingListService) {}
 
   ngOnInit(): void {}
 
@@ -30,7 +21,6 @@ export class ShoppingEditComponent implements OnInit {
     const ingAmount = this.amountInputRef.nativeElement.value;
     const newIngredient = new Ingredient(ingName, ingAmount);
 
-    // emit event
-    this.ingredientAdded.emit(newIngredient);
+    this.slService.addIngredient(newIngredient);
   }
 }
